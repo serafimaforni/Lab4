340e5498e45afe21c826654869be59b4f2f29516
Serafima Forni <sforni@carroll.edu>
2018-10-10 14:45:22 -0600

    changed message in branch A

--- myGitTest.java
+++ myGitTest.java
@@ -3,7 +3,7 @@ public class myGitTest {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Serafima says Hello Git Test");
+		System.out.println("Serafima says Hello Git Test Branch AAAAAAAA");
 
 	}
 
